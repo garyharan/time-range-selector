5c77cfbc5c4bccf66957c345439dacb8bebfb343
Gary Haran <gary.haran@gmail.com>
2008-11-25 20:46:16 -0500

    refactored the building of each div.

--- time_range.js
+++ time_range.js
@@ -4,32 +4,50 @@ var TimeRange = Class.create({
     this.options = Object.extend({
       squareSelectorSize: 15, 
       startTime: 8,
-      endTime: 20,
+      endTime: 19,
       interval: 30,
       hideField: false
     }, options);
     
-    this.buildTogglers();
-    if (this.options.hideField){ this.field.hide(); }
-  },
-  buildTogglers: function(){
-    var timeSlots = $R(this.options.startTime, this.options.endTime).collect(function(hour){
-      return [hour, $R(0, Math.floor(60 / this.options.interval)-1).collect(function(minute){
-        return minute * this.options.interval
-      }.bind(this))]
-    }.bind(this));
+    var tr = this;
     
-    var div = Builder.node('div');
-    div.setStyle({position: 'absolute', height: '30px'});
-    document.body.appendChild(div);
-    div.clonePosition(this.field, {setWidth: false, setHeight: false, offsetTop: -4, offsetLeft: this.field.offsetWidth + 10});
-    timeSlots.each(function(s){
-      s[1].each(function(slot) {
-        var slotDiv = Builder.node('div').setStyle({width: 20  + 'px', fontSize: '9px'});
-        slotDiv.update(s[0] + ":" + slot.paddedMinutes()).setStyle({float: 'left', height: '30px', fontSize: '8px', background: '#DDDDDD', border: '1px solid #D0D0D0'});
-        div.appendChild(slotDiv)
-      }.bind(this));
-    }.bind(this));
+    this.container = Builder.node('div');
+    document.body.appendChild(this.container);
+    this.container.setStyle({position: 'absolute', height: '30px'})
+      .clonePosition(this.field, {
+        setWidth: false, 
+        setHeight: false, 
+        offsetTop: -4, 
+        offsetLeft: this.field.offsetWidth + 10
+      });
+    
+    this.toggleTimeSlots = $R(this.options.startTime, this.options.endTime).collect(function(hour){
+      return $R(0, Math.floor(60 / tr.options.interval)-1).collect(function(minute){
+        return hour + '.' + (Number(minute * tr.options.interval) / 60 * 100)
+      }.bind(hour));
+    })
+    .flatten()
+    .reject(function(time){ return time > tr.options.endTime })
+    .collect(function(slot) {
+      var div = Builder.node('div').setStyle({
+        border: '1px solid #dfdfdf',
+        float: 'left',
+        margin: 'auto',
+        textAlign: 'center',
+        width: '21px',
+        height: '30px',
+        fontSize: '9px',
+        lineHeight: '30px',
+        background: '#f3f3f3'
+      }).update(slot % 1 ? '' : Number(slot).toHour());
+      div.setAttribute('title', Number(slot).toHour());
+      div.addClassName('slot');
+      return div;
+    }).each(function(slot) {
+      tr.container.appendChild(slot)
+    });
+    
+    if (this.options.hideField){ this.field.hide(); }
   }
 });
 
@@ -45,7 +63,10 @@ Object.extend(Number.prototype, {
     if (this > 24 || this < 0){ throw "Unrecognized Hour Exception" }
     var h = Math.floor(this);
     var m = parseFloat((this % 1) / 100 * 6000).toString();
-    return h + ":" + Math.round(m.length == 2 ? m : m + 0);
+    return h + ":" + (m.length == 2 ? m : m + 0);
+  },
+  toDecimalMinutes: function(){
+    return this / 6000 * 100
   },
   paddedMinutes: function() {
     if (this > 60 || this < 0){ throw "Unrecognized Minute Exception" }
